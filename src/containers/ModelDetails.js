2b92b40d4c346528b80ff156c0cd497c559cdb49
vicki alden <vic.alden23@gmail.com>
2017-09-10 16:12:21 -0700

    Set model detail to state

--- src/containers/ModelDetails.js
+++ src/containers/ModelDetails.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
+import {
+  GridList,
+  GridTile
+} from 'material-ui/GridList';
 
 class ModelDetails extends Component {
   constructor(props) {
     super(props);
+      this.state = {
+        model: {}
+      }
+  }
 
+  componentDidMount() {
+
+    let id = this.props.match.params.id;
+    axios.get('http://localhost:2023/api/listings')
+      .then(res => {
+        this.setState({
+          model: res.data.users[id]
+        });
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
+    let model = this.state.model;
     return (
-      <p>hello world</p>
+      <div>
+        {model.city_name}
+      </div>
     );
   }
 }
